7bb55d59e93bea07dc1a3f33f55b59fe7ad95302
deepakphpweb <dipakrana2760@gmail.com>
2021-12-20 22:06:43 +0530

    Pass lambda function to show the result

--- src/com/blz/lambda/IMathFunction.java
+++ src/com/blz/lambda/IMathFunction.java
@@ -2,5 +2,9 @@ package com.blz.lambda;
 
 @FunctionalInterface
 public interface IMathFunction {
+    static void printResult(int a, int b, String methodName, IMathFunction method) {
+        System.out.println("Result " +methodName + " is " +method.calculate(a,b));
+    }
+
     int calculate(int a, int b);
 }
--- src/com/blz/lambda/MathOperation.java
+++ src/com/blz/lambda/MathOperation.java
@@ -7,8 +7,12 @@ public class MathOperation {
         IMathFunction substract =(a,b) -> a-b;
         IMathFunction divide = (a,b) -> a/b;
 
-        System.out.println("Addition is "+add.calculate(2,3));
-        System.out.println("Substract is "+substract.calculate(3,2));
-        System.out.println("Division is "+divide.calculate(50,10));
+//        System.out.println("Addition is "+add.calculate(2,3));
+//        System.out.println("Substract is "+substract.calculate(3,2));
+//        System.out.println("Division is "+divide.calculate(50,10));
+
+        IMathFunction.printResult(5,6,"Addition",add);
+        IMathFunction.printResult(6,3, "Subtraction",substract);
+        IMathFunction.printResult(6,3,"Division",divide);
     }
 }
